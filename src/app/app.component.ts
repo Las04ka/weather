55aae82f03334011303f3dfda6b31c86f2bf5a15
Las04ka <denkostyuk@gmail.com>
2023-06-24 19:37:50 +0300

    made rendering data for one city

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cities } from 'src/app/constants/cities';
+import { IForecastResponse } from 'src/app/interfaces/forecast-response';
+import { WeatherService } from 'src/app/services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  cities = Cities;
+  forecastResponse?: IForecastResponse;
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.weatherService
+      .getWeather(this.cities[0])
+      .subscribe((el) => (this.forecastResponse = el));
+  }
 }
---
+++ src/app/constants/cities.ts
@@ -0,0 +1,1 @@
+export const Cities = ['London', 'Prague', 'Kyiv', 'Buenos Aires'];
